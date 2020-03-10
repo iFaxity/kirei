6e375fc9a401f203045d175afb562b9d60342233
iFaxity <izze96@gmail.com>
2020-03-10 11:04:11 +0100

    Removed unnecessary files

--- packages/element/src/queue.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function nextTick(fn?: () => void): Promise<void>;
-export declare function has(fn: Function): boolean;
-export declare function push(fn: Function): void;
-//# sourceMappingURL=queue.d.ts.map
