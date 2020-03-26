1cac5559bddbe86398be639bf5d65f1e4bc23c92
iFaxity <izze96@gmail.com>
2020-03-27 00:36:48 +0100

    refactor: code refactor in parsers (html)

--- packages/html/src/parser.ts
+++ packages/html/src/parser.ts
@@ -1,6 +1,7 @@
-import { toRawValue } from '@shlim/fx';
+import { isRef } from '@shlim/fx';
 import { diff } from './shared';
 import { parseDirective, DirectiveUpdater } from './directive';
+import { warn } from '@shlim/shared/src';
 
 /**
  * Maps a style or class attribute from an array or an object to a string
@@ -22,88 +23,92 @@ export function attrParser(node: Element, name: string): DirectiveUpdater {
   if (res) return res;
 
   // Default to attribute parsing
-  const mapValue = name == 'class' || name == 'style';
+  const shouldMap = name == 'class' || name == 'style';
   let value;
+  let mounted = false;
+
   return (pending: any) => {
-    let newValue = toRawValue(pending) as any;
-    if (value === newValue) return;
+    const newValue = isRef(pending) ? pending.value : pending;
+    if (value === newValue && !shouldMap) return;
     value = newValue;
 
-    if (value == null) {
-      node.removeAttribute(name);
-    } else {
-      node.setAttribute(name, mapValue ? mapAttribute(value) : value);
+    if (value == null || value == '') {
+      if (mounted) {
+        node.removeAttribute(name);
+        mounted = false;
+      }
+    } else if (!mounted) {
+      node.setAttribute(name, shouldMap ? mapAttribute(value) : value);
+      mounted = true;
     }
   };
 }
 
 export function nodeParser(refNode: Comment): DirectiveUpdater {
-  let nodes = [];
+  let nodes: Node[] = [];
   let value;
-  let text;
+  let text: Text;
 
-  const parse = (pending: any) => {
-    const newValue = toRawValue(pending) as any;
-    switch (typeof newValue) {
+  function nodeTextParser(newValue: any) {
+    if (value === newValue) return;
+    value = newValue;
+
+    if (!text) {
+      text = document.createTextNode(value);
+    } else {
+      text.textContent = value;
+    }
+
+    nodes = diff(refNode, nodes, [text]);
+  }
+
+  return (pending: any) => {
+    const newValue = isRef(pending) ? pending.value : pending;
+    const type = typeof newValue;
+
+    if (type != 'object' && type != 'undefined') {
       // primitives are handled as text content
-      case 'string':
-      case 'number':
-      case 'boolean':
-        if (value !== newValue) {
-          value = newValue;
-          if (!text) {
-            text = document.createTextNode('');
-          }
-
-          text.textContent = value;
-          nodes = diff(refNode, nodes, [text]);
-        }
-        break;
+      nodeTextParser(newValue);
+    } else if (newValue == null) {
       // null, and undefined are used to cleanup previous content
-      case 'object':
-      case 'undefined':
-        if (newValue == null) {
-          if (value) {
-            value = newValue;
-            nodes = diff(refNode, nodes, []);
-          }
-        } else if (Array.isArray(newValue)) {
-          // arrays and nodes have a special treatment
-          value = newValue;
-          if (value.length === 0) {
-            // arrays can be used to cleanup, if empty
-            nodes = diff(refNode, nodes, []);
-          } else if (typeof value[0] === 'object') {
-            // or diffed, if these contains nodes or "wires"
-            nodes = diff(refNode, nodes, value);
-          } else {
-            // in all other cases the content is stringified as is
-            parse('' + value);
-          }
-        } else if ('ELEMENT_NODE' in newValue && newValue !== value) {
-          // if the new value is a DOM node, or a wire, and it's
-          // different from the one already live, then it's diffed.
-          // if the node is a fragment, it's appended once via its childNodes
-          // There is no `else` here, meaning if the content
-          // is not expected one, nothing happens, as easy as that.
-          if (newValue.nodeType === Node.DOCUMENT_FRAGMENT_NODE) {
-            value = Array.from((newValue as DocumentFragment).childNodes);
-          } else {
-            value = [newValue];
-          }
-          nodes = diff(refNode, nodes, value);
-        }
+      if (value) {
+        value = newValue;
+        nodes = diff(refNode, nodes, []);
       }
-  };
+    } else if (Array.isArray(newValue)) {
+      // arrays and nodes have a special treatment
+      value = newValue;
 
-  return parse;
+      if (value.length === 0 || typeof value[0] === 'object') {
+        // arrays can be used to cleanup, if empty
+        // or diffed, if these contains nodes or "wires"
+        nodes = diff(refNode, nodes, value);
+      } else {
+        // in all other cases the content is stringified as is
+        nodeTextParser(newValue.toString());
+      }
+    } else if ('ELEMENT_NODE' in newValue && newValue !== value) {
+      // if the new value is a DOM node, or a wire, and it's
+      // different from the one already live, then it's diffed.
+      // if the node is a fragment, it's appended once via its childNodes
+      // There is no `else` here, meaning if the content
+      // is not expected one, nothing happens, as easy as that.
+      value = newValue.nodeType === Node.DOCUMENT_FRAGMENT_NODE
+        ? Array.from((newValue as DocumentFragment).childNodes)
+        : [newValue];
+      nodes = diff(refNode, nodes, value);
+    } else {
+      warn('Invalid node expression in html template');
+    }
+  };
 }
 
 export function textParser(node: Text): DirectiveUpdater {
   let value;
 
   return (pending: any) => {
-    const newValue = toRawValue(pending) as any;
+    const newValue = isRef(pending) ? pending.value : pending;
+
     if (value !== newValue) {
       value = newValue;
       node.textContent = value == null ? '' : value;
