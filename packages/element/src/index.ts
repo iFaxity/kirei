34e3371e2938e6518131e3348215250be57bf27e
iFaxity <izze96@gmail.com>
2020-03-27 00:33:43 +0100

    cleanup: removed old code in comments (element)

--- packages/element/src/index.ts
+++ packages/element/src/index.ts
@@ -4,29 +4,12 @@ export { defineElement, FxElement, elementInstances } from './instance';
 export { nextTick } from './queue';
 export { css } from './css';
 export * from './lifecycle';
+export * from './directive';
 
-
-// import directives
+// define default directives
 import './directives/bind';
 import './directives/conditional';
 import './directives/on';
 import './directives/ref';
 import './directives/show';
 import './directives/sync';
-
-/**
- * Creates a TemplateResult to cache markup, parses content as normal html
- * @param {TemplateStringsArray} strings Template strings
- * @param {*} values Template values
- * @returns {TemplateResult}
- *
-export const html = (strings, ...values) => new TemplateResult(strings, values, 'html', templateProcessor);
-
-/**
- * Creates a TemplateResult to cache markup, but parses content within an SVG namespace
- * @param {TemplateStringsArray} strings Template strings
- * @param {*} values Template values
- * @returns {TemplateResult}
- *
-export const svg = (strings, ...values) => new SVGTemplateResult(strings, values, 'svg', templateProcessor);
-*/
