502eafbd639d5c03f5f0fbf8e9c5d1435be3697b
iFaxity <izze96@gmail.com>
2020-04-05 13:39:50 +0200

    Publishing script updated (root)

--- publish.js
+++ publish.js
@@ -1,64 +1,110 @@
+#!/usr/bin/env node
 // yarn publish all packages with same version
 // create a git commit and tag it
 const { promisify } = require('util');
 const fs = require('fs');
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
-const writeFile = promisify(fs.writeFile);
 const { exec } = require('child_process');
 const path = require('path');
 
 const ROOT_DIR = __dirname;
 const PACKAGES_DIR = path.join(ROOT_DIR, 'packages');
-const pkg = require('./package.json');
+const rootPkg = require('./package.json');
 const semver = [ 'major', 'minor', 'patch' ];
+const pkgPrefix = '@shlim/';
 
-function shell(cmd, package) {
-  const cwd = package ? path.join(PACKAGES_DIR, package) : ROOT_DIR;
-
+// json file operations
+function writeJSON(filepath, data) {
   return new Promise((resolve, reject) => {
+    const json = JSON.stringify(data, null, 2);
+    fs.writeFile(filepath, json, 'utf8', (ex) => ex ? reject(ex) : resolve());
+  });
+}
+function readJSON(filepath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, 'utf8', (ex, data) => ex ? reject(ex) : resolve(JSON.parse(data)));
+  });
+}
+
+// Executes a shell command
+function shell(cmd, package) {
+  return new Promise((resolve, reject) => {
+    const cwd = package ? path.join(PACKAGES_DIR, package) : ROOT_DIR;
     exec(cmd, { cwd, encoding: 'utf-8' }, ex => ex ? reject(ex) : resolve());
   });
 }
 
+// Bumps the version from the root package
 function bumpVersion(tag) {
   const idx = semver.indexOf(tag);
-  const version = pkg.version.split('.');
-
-  if (idx == -1) {
-    throw new Error('');
+  const version = rootPkg.version.split('.').map(Number);
+  // We need fallthrough
+  switch(idx) {
+    case 0:
+      version[0] += 1;
+    case 1:
+      version[1] = idx == 1 ? version[1] + 1 : 0;
+    case 2:
+      version[2] = idx == 2 ? version[2] + 1 : 0;
+      break;
+    default:
+      throw new Error('Version tag not recognised');
   }
-
-  version[idx] = String((Number(version[idx]) + 1));
   return version.join('.');
 }
 
+// Assures that all linked dependencies gets bumped too
+async function bumpDependencies(pkg, version) {
+  const keys = [ 'dependencies', 'peerDependencies', 'devDependencies'];
+
+  for (const key of keys) {
+    const deps = pkg[key];
+    if (!deps) continue;
+
+    for (const dep of Object.keys(deps)) {
+      if (dep.startsWith(pkgPrefix)) {
+        deps[dep] = `^${version}`;
+      }
+    }
+  }
+
+  const package = pkg.name.split('/')[1];
+  await writeJSON(path.join(PACKAGES_DIR, package, 'package.json'), pkg);
+}
+
+// Program entrypoint
 async function main() {
-  const tag = process.argv[3];
+  const tag = process.argv[2];
   const version = bumpVersion(tag);
   const packages = await readdir(PACKAGES_DIR);
 
   // Do yarn publish on all packages individually
-  // THis assumes prepublish script hook for rebuilding the typescript sources
-  console.log('Publishing package');
+  // This assumes prepublish script hook for rebuilding the typescript sources
+  console.log(`Next version v${version}`);
   for (const package of packages) {
-    const dir = path.join(PACKAGES_DIR, dir);
+    const dir = path.join(PACKAGES_DIR, package);
     const stats = await stat(dir);
 
+    // Only publish directories with package.json set to public
     if (stats.isDirectory()) {
-      await shell(`yarn publish --new-version ${version} --no-git-tag-version --access=public`, package);
+      const pkg = await readJSON(path.join(dir, 'package.json'));
+
+      if (pkg.private !== true) {
+        console.log(`Publishing package ${package}`);
+        await bumpDependencies(pkg, version);
+        await shell(`yarn publish --new-version ${version} --no-git-tag-version --access public`, package);
+      }
     }
   }
 
   // Update version in root package
-  pkg.version = version;
-  await writeFile(path.join(ROOT_DIR, 'package.json'), JSON.stringify(pkg), 'utf8');
+  rootPkg.version = version;
+  await writeJSON(path.join(ROOT_DIR, 'package.json'), rootPkg);
 
   // Create a commit with a release tag
-  console.log('Creating commit');
-  await shell(`git commit -m v${version}`);
+  await shell(`git commit -am v${version}`);
   await shell(`git tag -a v${version} -m v${version}`);
 }
 
-main.catch(console.error);
-
+main().catch(console.error);
