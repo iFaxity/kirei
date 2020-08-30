074ca3b44dab8d8316da93e61bf46341a28786df
iFaxity <izze96@gmail.com>
2020-08-30 18:33:05 +0200

    Updated vite-plugin-istanbul

--- cypress/app/vite.config.js
+++ cypress/app/vite.config.js
@@ -4,7 +4,6 @@ const istanbul = require('vite-plugin-istanbul');
 const include = [ 'elements/', 'views/' ];
 
 module.exports = {
-  open: true,
   port: 3000,
   plugins: [ kirei({ include, cwd: __dirname }), istanbul({ exclude: [ 'cypress' ] }), ],
 };
