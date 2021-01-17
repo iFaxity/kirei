e5616ae4f19aa3def46535d04699a4874c9806ce
iFaxity <izze96@gmail.com>
2021-01-17 23:22:41 +0100

    break(shared): Removed duplicate methods from @vue/shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,16 +1,3 @@
-// https://tc39.github.io/ecma262/#sec-typeof-operator
-export type Primitive = null|undefined|boolean|number|string|symbol|bigint;
-
-/**
- * Checks if a variable is a primitive value (not object or function)
- * @param target - Target to check
- * @returns True if target is a primitive value
- */
-export function isPrimitive(target: unknown): target is Primitive {
-  const type = typeof target;
-  return target == null || type != 'object' && type != 'function';
-}
-
 /**
  * Checks if a value is an object (i.e not a primitive)
  * @param target - Target to check
@@ -21,39 +8,12 @@ export function isObject<T = object>(target: any): target is T {
 }
 
 /**
- * Checks if a value is a function
- * @param target - Target to check
- * @returns True if target is a function
- */
-export function isFunction(target: any): target is Function {
-  return typeof target == 'function';
-}
-
-/**
- * Checks if a value is a string
- * @param target - Target to check
- * @returns True if target is a string
- */
-export function isString(target: any): target is string {
-  return typeof target == 'string';
-}
-
-/**
  * Checks if a value is undefined
  * @param target - Target to check
  * @returns True if target is undefined
  */
 export function isUndefined(target: any): target is undefined {
   return typeof target == 'undefined';
-}
-
-/**
- * Checks if a value is a Promise or a PromiseLike, aka an object with a then function.
- * @param target - Target to check
- * @returns True if target is a Promise or a PromiseLike
- */
-export function isPromise<T = any>(target: any|PromiseLike<T>): target is Promise<T> {
-  return typeof target?.then == 'function';
 }
 
 /**
