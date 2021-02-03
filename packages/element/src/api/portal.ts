aacb267f9da416a1470fc5ac77816705a166ae9e
iFaxity <izze96@gmail.com>
2021-02-03 21:27:18 +0100

    fix(element): Added experimental portal component (v-portal)

--- packages/element/src/api/portal.ts
+++ packages/element/src/api/portal.ts
@@ -1,54 +1,23 @@
-import { effect, stop } from '@vue/reactivity';
-import type { ReactiveEffect } from '@vue/reactivity';
-import { Template } from '@kirei/html';
-import { render } from '../runtime/compiler';
-import { onUnmount } from './lifecycle';
-import * as Queue from '../runtime/queue';
-import { getCurrentInstance, setCurrentInstance } from '../runtime/instance';
-import type { ComponentInstance } from '../types';
+import { onMount } from './lifecycle';
+import { getCurrentInstance } from '../runtime/instance';
+import { defineComponent } from './defineComponent';
 
-const roots = new Map<string, Element>();
-const portals = new WeakMap<Element, Portal>();
-interface Portal {
-  instance: ComponentInstance;
-  fx: ReactiveEffect;
-}
+defineComponent({
+  name: 'VPortal',
+  setup() {
+    const { el } = getCurrentInstance();
+    const $body = window.document.body;
+    const $slot = document.createElement('slot');
 
-// TODO: maybe clean up this code, and change it up a bit
-
-/**
- * Portals content to a element, useful for dialogs
- * @param target - Target element as querySelector string
- * @param template - Template to render
- */
-export function portal(target: string, templateFn: () => Template): void {
-  const instance = getCurrentInstance();
-  let root = roots.get(target);
-  if (!root) {
-    roots.set(target, (root = document.querySelector(target)));
-  }
-
-  let portal = portals.get(root);
-  if (!portal || portal.instance !== instance) {
-    const fx = effect(() => {
-      setCurrentInstance(instance);
-      render(templateFn(), root);
-      setCurrentInstance(null);
-    }, { scheduler: Queue.push });
-
-    portal = { instance, fx };
-    onUnmount(() => {
-      const p = portals.get(root);
-      stop(fx);
-
-      if (p === portal) {
-        render(null, root);
-        portals.delete(root);
+    // Ensure the elements parent is the body
+    onMount(() => {
+      if (el.parentNode != $body) {
+        $body.appendChild(el);
       }
     });
 
-    portals.set(root, portal);
-  }
-}
-
-
+    // patching should already be done by the parent component
+    // this element just moves the elements to another root
+    return () => $slot;
+  },
+});
